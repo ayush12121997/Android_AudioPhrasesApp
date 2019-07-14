0d14b41286b6b5e8dcffb1d33d5cf45634c31147
Ayush Shrivastava <ayush16024@iiitd.ac.in>
2019-07-14 14:20:03 +0530

    Completed app. Added all sound effects for english to french.

--- app/src/main/java/com/example/ayush/audiophrasesapp/MainActivity.java
+++ app/src/main/java/com/example/ayush/audiophrasesapp/MainActivity.java
@@ -1,9 +1,57 @@
 package com.example.ayush.audiophrasesapp;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    MediaPlayer mediaPlayer;
+
+    public void playPhrase(View view)
+    {
+        if(view.getTag().equals("Name"))
+        {
+            mediaPlayer = MediaPlayer.create(this, R.raw.mynameis);
+            mediaPlayer.start();
+        }
+        else if(view.getTag().equals("How"))
+        {
+            mediaPlayer = MediaPlayer.create(this, R.raw.howareyou);
+            mediaPlayer.start();
+        }
+        else if(view.getTag().equals("Hello"))
+        {
+            mediaPlayer = MediaPlayer.create(this, R.raw.hello);
+            mediaPlayer.start();
+        }
+        else if(view.getTag().equals("Please"))
+        {
+            mediaPlayer = MediaPlayer.create(this, R.raw.please);
+            mediaPlayer.start();
+        }
+        else if(view.getTag().equals("Welcome"))
+        {
+            mediaPlayer = MediaPlayer.create(this, R.raw.welcome);
+            mediaPlayer.start();
+        }
+        else if(view.getTag().equals("Evening"))
+        {
+            mediaPlayer = MediaPlayer.create(this, R.raw.goodevening);
+            mediaPlayer.start();
+        }
+        else if(view.getTag().equals("Live"))
+        {
+            mediaPlayer = MediaPlayer.create(this, R.raw.ilivein);
+            mediaPlayer.start();
+        }
+        else
+        {
+            mediaPlayer = MediaPlayer.create(this, R.raw.doyouspeakenglish);
+            mediaPlayer.start();
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
