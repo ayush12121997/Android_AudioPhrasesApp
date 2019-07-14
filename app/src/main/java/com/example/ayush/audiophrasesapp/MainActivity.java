c48dd19bbc534088237ca8c5b56bcae661adaec3
Ayush Shrivastava <ayush16024@iiitd.ac.in>
2019-07-14 17:19:01 +0530

    Improvised the app to enable it to play the media files even after several attempts.

--- app/src/main/java/com/example/ayush/audiophrasesapp/MainActivity.java
+++ app/src/main/java/com/example/ayush/audiophrasesapp/MainActivity.java
@@ -3,54 +3,27 @@ package com.example.ayush.audiophrasesapp;
 import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    MediaPlayer mediaPlayer;
+    public void playPhrase(View view) {
 
-    public void playPhrase(View view)
-    {
-        if(view.getTag().equals("Name"))
-        {
-            mediaPlayer = MediaPlayer.create(this, R.raw.mynameis);
-            mediaPlayer.start();
-        }
-        else if(view.getTag().equals("How"))
-        {
-            mediaPlayer = MediaPlayer.create(this, R.raw.howareyou);
-            mediaPlayer.start();
-        }
-        else if(view.getTag().equals("Hello"))
-        {
-            mediaPlayer = MediaPlayer.create(this, R.raw.hello);
-            mediaPlayer.start();
-        }
-        else if(view.getTag().equals("Please"))
-        {
-            mediaPlayer = MediaPlayer.create(this, R.raw.please);
-            mediaPlayer.start();
-        }
-        else if(view.getTag().equals("Welcome"))
-        {
-            mediaPlayer = MediaPlayer.create(this, R.raw.welcome);
-            mediaPlayer.start();
-        }
-        else if(view.getTag().equals("Evening"))
-        {
-            mediaPlayer = MediaPlayer.create(this, R.raw.goodevening);
-            mediaPlayer.start();
-        }
-        else if(view.getTag().equals("Live"))
-        {
-            mediaPlayer = MediaPlayer.create(this, R.raw.ilivein);
-            mediaPlayer.start();
-        }
-        else
-        {
-            mediaPlayer = MediaPlayer.create(this, R.raw.doyouspeakenglish);
-            mediaPlayer.start();
-        }
+        Button buttonPressed = (Button) view;
+
+        Log.i("Button pressed", buttonPressed.getTag().toString());
+
+        MediaPlayer mediaPlayer = MediaPlayer.create(this, getResources().getIdentifier(buttonPressed.getTag().toString(), "raw", getPackageName()));
+        mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mediaPlayer) {
+                mediaPlayer.release();
+            }
+        });
+        mediaPlayer.start();
+
     }
 
     @Override
